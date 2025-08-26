ba3d1081a1d5a8c748cf3aa8336b024157aaae1f
TE24B-Mia-Hindersson <mia@hindersson.com>
2025-08-26 14:14:45 +0200

    intial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿string svar;
+Console.WriteLine("Vad heter du?");
+svar = Console.ReadLine();
+string svartwo;
+Console.WriteLine("Snälla säg en färg");
+svartwo = Console.ReadLine();
+string svarthree;
+Console.WriteLine("Snälla säg en mat");
+svarthree = Console.ReadLine();
+string svarfour;
+Console.WriteLine("Snälla säg varför du valde den maten");
+svarfour = Console.ReadLine();
+Console.WriteLine($"Snart ska {svar} plocka upp en {svartwo} färgad {svarthree} för att {svarfour}");
+Console.ReadLine();
+
+
+
+
+
+
+
+// string name;
+// Console.WriteLine("vad heter du?");
+// name = Console.ReadLine();
+// string positivtOrd = "snällt";
+// Console.WriteLine("Hello");
+// Console.WriteLine($"tack {name} så {positivtOrd}");
+                                             
+// Console.ReadLine();
