0923763bf32ef49341923ea58ee80a8c5a38eb51
TE24B-Mia-Hindersson <mia@hindersson.com>
2026-03-03 15:13:55 +0100

    name

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("begining");
+﻿string name = "";
+while (name.Length <= 1 || name.Length >= 12)
+{
+    Console.WriteLine("Name Plsssssssssssssssssssssss");
+    name = Console.ReadLine();
+    if (name.Length <= 1)
+    {
+        Console.WriteLine(",,");
+    }
+    if (name.Length >= 12)
+    {
+        Console.WriteLine("....");
+    }
+    Console.ReadLine();
+    Console.Clear();
+}
+
+
