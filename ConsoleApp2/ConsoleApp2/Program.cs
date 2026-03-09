205af603493b0e58886ac9b6606a0458735ddfdd
TE24B-Mia-Hindersson <mia@hindersson.com>
2026-03-09 09:04:38 +0100

    stats

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -14,5 +14,14 @@ while (name.Length <= 1 || name.Length >= 12)
     Console.ReadLine();
     Console.Clear();
 }
+int stats = 0;
+while (stats < 10)
+{
+    // Console.WriteLine($"points? \n1) Strength \n2) Perception \n3) Endurance \n4) Charisma \n5) Intelligence \n6) Agility \n7) Luck ");
+    string answer = Console.ReadLine();
+if (answer == "1"); 
 
+}
+
+Console.ReadLine();
 
